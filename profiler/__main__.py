b77c75996e2e642e36bca388ce9f07c160a941ca
kremrik <kyle.robert.e@gmail.com>
2021-11-10 19:18:04 -0600

    cli for profiler

--- profiler/__main__.py
+++ profiler/__main__.py
@@ -1,11 +1,61 @@
+from table import Table
 from utils.cli import Arg, cli
 
+from dataclasses import dataclass
+from os.path import abspath, dirname, join
+from random import randint, seed
+from string import ascii_lowercase
 from sys import argv
 
 
+seed(13)
+
+
+HERE = dirname(abspath(__file__))
+DB_NAME = join(HERE, "profiler.db")
+
+
+@dataclass
+class Foo:
+    letters: str
+    number: int
+
+
+def make_db():
+    rows = 1_000
+    table = Table(Foo, DB_NAME)
+
+    rand_l = randint(1, 10)
+    letters = "".join(_alpha_rand() for _ in range(rand_l))
+    number = randint(1, 10000)
+
+    records = []
+    for _ in range(rows):
+        rand_l = randint(1, 10)
+        letters = "".join(_alpha_rand() for _ in range(rand_l))
+        number = randint(1, 10000)
+        record = Foo(letters, number)
+        records.append(record)
+    
+    table.insert(records)
+
+
+def _alpha_rand() -> str:
+    rand = randint(0, 25)
+    return ascii_lowercase[rand]
+
+
 opts = [
-    Arg("setup", lambda: print("SETUP"), "Set up DB"),
-    Arg("teardown", lambda: print("TEARDOWN"), "Tear down db")
+    Arg(
+        "setup", 
+        make_db, 
+        "Set up DB"
+    ),
+    Arg(
+        "teardown", 
+        lambda: print("TEARDOWN"), 
+        "Tear down db"
+    )
 ]
 
 
