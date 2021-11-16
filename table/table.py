9b2366520f15d06a7b87467cb4781e8fbe0e47aa
kremrik <kyle.robert.e@gmail.com>
2021-11-15 20:52:49 -0600

    splitting up _setup method

--- table/table.py
+++ table/table.py
@@ -86,17 +86,7 @@ class Table:
         return self._db.execute(querystring, variables)
 
     def _start(self):
-        mmap_size = None
-        if self.location and exists(self.location):
-            db_size = getsize(self.location)
-            mmap_size = (
-                round((db_size / 1024) * 1.5) * 1024
-            )
-            LOGGER.debug(f"db size: [{db_size}]")
-            LOGGER.debug(f"mmap_size: [{mmap_size}]")
-
-        db = Database(self.location, mmap_size)
-        self._db = db
+        self._create_db()
 
         try:
             self._db.create_table(
@@ -119,6 +109,19 @@ class Table:
         LOGGER.debug(
             f"Table created with model [{self.dclass}]"
         )
+
+    def _create_db(self):
+        mmap_size = None
+        if self.location and exists(self.location):
+            db_size = getsize(self.location)
+            mmap_size = (
+                round((db_size / 1024) * 1.5) * 1024
+            )
+            LOGGER.debug(f"db size: [{db_size}]")
+            LOGGER.debug(f"mmap_size: [{mmap_size}]")
+
+        db = Database(self.location, mmap_size)
+        self._db = db
 
 
 # ---------------------------------------------------------
